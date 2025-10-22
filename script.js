bf9d037f987ce9971a14f80e33759adda8bf1b5a
Denis228-pro <64372060+Denis228-pro@users.noreply.github.com>
2025-10-22 18:43:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let currentPage = 'catalog';
 let deliveryAddress = '';
 
 // Backend configuration - ЗАМЕНИТЕ НА ВАШ РЕАЛЬНЫЙ URL
-const BACKEND_URL = 'https://script.google.com/macros/s/AKfycbyTNPIj-9iPUy4igIHziWrFheSK1DBGKvgK5EJODCCDI0Mfl6qqsLX61THuWKJC02c4/exec';
+const BACKEND_URL = 'https://script.google.com/macros/s/AKfycbyUv3xnxCjRYWVf1HldFvKUdLWl92JVTTFHB4I04qXTDWvizqGNrdQMJ01BJgW3sYVy/exec';
 
 // Initialize the app
 async function initApp() {
@@ -1286,6 +1286,7 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log('DOM fully loaded, initializing app...');
     initApp();
 });
+
 
 
 
